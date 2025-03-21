045d5a1c82bb59d573a1f1433d6f882ef8e07f1a
大沼 峻徳 <numa.san.1024@gmail.com>
2025-03-21 15:52:23 +0900

    PWAの設定をした

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   server: {
     host: true,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        lang: "ja",
+        name: "音楽プレーヤー",
+        short_name: "Music",
+        background_color: "#fff",
+        theme_color: "#2196f3",
+        display: "standalone",
+        // GitHub Pagesのリポジトリ名を指定
+        start_url: "/music-player/",
+        scope: "/music-player/",
+        icons: [
+          {
+            src: "app-icons/512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
